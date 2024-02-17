f76a9ae0aae8b4fba6dbf333390d8e61e7a01582
pavan awachar <147679047+pavanawachar@users.noreply.github.com>
2024-02-17 15:37:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,59 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+
+  arr.map(student => {
+    if(student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
+
+PrintStudentswithMap();
+
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  
+
+  arr.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
+
+PrintStudentsbyForEach();
 
 function addData() {
   //Write your code here, just console.log
+ 
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr);
 }
+addData();
+
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  
+  arr.filter(student => {
+    if(student.marks > 50) {
+      console.log(student);
+    }
+  });
+  
 }
+removeFailedStudent();
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+  { id: 5, name: "bigg", age: "33", marks: 30 },
+  { id: 6, name: "boss", age: "43", marks: 60 },
+  { id: 7, name: "season", age: "53", marks: 90 },
+  ];
+  
+  console.log(arr.concat(arr2));
 }
+concatenateArray();
